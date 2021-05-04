5b843aad21edfa1e5462f7c74f9bc02aa2b3606e
Will R <williamp.robertson@gmail.com>
2021-05-03 18:04:37 -0700

    v0.1

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+
+
+
 const tmi = require('tmi.js');
 
 const client = new tmi.Client({
@@ -16,17 +20,62 @@ const client = new tmi.Client({
 
 client.connect();
 
+// client.on('message', async (channel, context, message) => {
+//   const isNotBot = context.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
+
+//   if ( !isNotBot ) return;
+
+//   const [raw, command, argument] = message.match(regexpCommand);
+
+//   const { response } = commands[command] || {};
+
+//   let responseMessage = response;
+
+//   if ( typeof responseMessage === 'function' ) {
+//     responseMessage = response(argument);
+//   }
+
+//   if ( responseMessage ) {
+//     console.log(`Responding to command !${command}`);
+//     client.say(channel, responseMessage);
+//   }
+
+// });
+
 client.on('message', (channel, tags, message, self) => {
-  // const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME
+	if(self || !message.startsWith('!')) return;
 
-  // if (isNotBot) {
-  //   client.say(channel, `Message "${message}" was sent by ${tags.username}`);
-  // }
-	// Ignore echoed messages.
-	if(self) return;
+	const args = message.slice(1).split(' ');
+	// const command = args.shift().toLowerCase();
+  const commands = {
+    links: {
+      response: 'Follow me on Twitter https://twitter.com/UlrichTyson and Instagram https://www.instagram.com/t_bone1701/'
+    },
+    RW: {
+      response: (argument) => `that's right ${argument} its time to RUN WILD!`
+    },
+    break: {
+      response: 'test break messsage'
+    }
+  }
+  
 
-	if(message.toLowerCase() === '!hello') {
-		// "@alca, heya!"
-		client.say(channel, `@${tags.username}, heya!`);
-	}
+	// if(command === 'echo') {
+	// 	client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`);
+	// }
+
+  const [raw, command, argument] = message.match(regexpCommand);
+
+  const { response } = commands[command] || {};
+
+  let responseMessage = response;
+
+  if ( typeof responseMessage === 'function' ) {
+    responseMessage = response(argument);
+  }
+
+  if ( responseMessage ) {
+    console.log(`Responding to command !${command}`);
+    client.say(channel, responseMessage);
+  }
 });
