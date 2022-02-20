d27e3dec38570fb61dc274008f017db570053845
Will R <williamp.robertson@gmail.com>
2022-02-19 22:08:42 -0700

    updated spin

--- server.js
+++ server.js
@@ -250,7 +250,7 @@ client.on('message', (channel, tags, message, self) => {
       response: `🥩 oh yeah @${tags.username}, its 🎉SUB HYPE🎉 time! 🙏🏻 Thank you ${args[1]} for supporting the channel, it means a lot. As always, lets RUN WILD! 🎉SUB HYPE🎉🎉SUB HYPE🎉🎉SUB HYPE🎉 🥩`
     },
     spin: {
-      response: `🥩 The inaugural winner of T Bone1701's affiliate challenge is...  ${contest}. Thanks for the support everyone! Keep an eye on the channel for the next community challenge 💪🏻 and more giveaways! #runninwild 🥩`
+      response: `🥩 The inaugural winner of T Bone1701's affiliate challenge is...  ${contest} (you will receive a dm soon). Thanks for the support everyone! Keep an eye on the channel for the next community challenge 💪🏻 and more giveaways! #runninwild 🥩`
     },
     mmm: {
       response: `🍕 @${tags.username} says deliver me some Gallucci's 🍕`
